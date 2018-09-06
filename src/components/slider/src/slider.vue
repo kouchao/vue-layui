11ffd06af667a028559715440d0d09973897a205
kouchao <admin@jskou.com>
2018-09-06 15:47:46 +0800

    新增 滑块组件，滑块demo

--- src/components/slider/src/slider.vue
+++ src/components/slider/src/slider.vue
@@ -1,35 +1,72 @@
 <template>
-    <div class="layui-slider" ref="slider">
-        <div class="layui-slider-tips"
-             style="display: inline-block"
-             v-if="isShowTip"
-             :style="{
-                left: leftP + '%'
-             }">
-            {{_value}}
+    <div style="position: relative;">
+        <div class="layui-slider"
+             :class="{'layui-slider-vertical': vertical, 'layui-disabled': disabled}"
+             :style="vertical ? 'height: ' + height + 'px': ''"
+             ref="slider">
+            <div class="layui-slider-tips"
+                 :class="{
+                    'is-vertical': vertical
+                 }"
+
+                 style="display: inline-block"
+                 v-if="isShowTip && showTip "
+                 :style="(vertical ? 'bottom: ' + tipLeft + '%' : 'left: ' + tipLeft + '%')">
+
+                <slot :value="tipVal">{{tipVal}}</slot>
+            </div>
+            <div class="layui-slider-bar"
+                 :class="'layui-bg-' + this.theme"
+                 :style="(vertical ? 'bottom: ' + left + '%' : 'left: ' + left + '%') + '; ' + (vertical ? 'height: ' + width + '%' : 'width: ' + width + '%')">
+            </div>
+            <lay-slider-btn v-if="isArray" v-model="startValue"
+                            :max="max"
+                            :min="min"
+                            :step="step"
+                            :steps="steps"
+                            :slider-width="sliderWidth"
+                            @tip="showOrHideTip"
+                            :vertical="vertical"
+                            :theme="theme"
+                            :disabled="disabled">
+            </lay-slider-btn>
+            <lay-slider-btn v-model="endValue"
+                            :max="max"
+                            :min="min"
+                            :step="step"
+                            :steps="steps"
+                            :slider-width="sliderWidth"
+                            @tip="showOrHideTip"
+                            :vertical="vertical"
+                            :theme="theme"
+                            :disabled="disabled">
+            </lay-slider-btn>
+            <div v-if="showStops" v-for="item in steps" class="layui-slider-step" :style="(vertical ? 'bottom' : 'left') + ': ' + item + '%'"></div>
         </div>
-        <div class="layui-slider-bar" :style="{
-            background: 'rgb(0, 150, 136)',
-            width: leftP + '%',
-            left: '0%'
-        }"></div>
-        <div class="layui-slider-wrap"
-             :style="{
-                left: leftP + '%'
-             }">
-            <div class="layui-slider-wrap-btn"
-                 style="border: 2px solid #009688;"
-                 @mousedown="onDragStart"
-                 @mouseenter="handleMouseEnter"
-                 @mouseleave="handleMouseLeave"></div>
+        <div v-if="!isArray && showInput" class="layui-slider-input layui-input"
+             :class="{'is-vertical': vertical}"
+             @mouseenter="showInputBtn = true"
+             @mouseleave="showInputBtn = false">
+            <div class="layui-slider-input-txt">
+                <input v-model="endValue" type="text" @change="handleChange" class="layui-input">
+            </div>
+            <div class="layui-slider-input-btn" v-if="showInputBtn">
+                <i class="layui-icon layui-icon-up" @click="handleUp"></i>
+                <i class="layui-icon layui-icon-down" @click="handleDown"></i>
+            </div>
         </div>
     </div>
+
 
 </template>
 
 <script>
+    import LaySliderBtn from './slider-btn'
 	export default {
 		name: 'LaySlider',
+        components: {
+	        LaySliderBtn
+        },
 		props: {
 			value: {
 				type: [Number, Array],
@@ -42,84 +79,154 @@
 			min: {
 				type: Number,
 				default: () => 0
-			}
+			},
+			step: {
+				type: Number,
+                default: () => 1
+            },
+			showStops: Boolean,
+			showTip: {
+				type: Boolean,
+				default: () => true
+			},
+			showInput: {
+				type: Boolean,
+				default: () => false
+			},
+			vertical: {
+				type: Boolean,
+				default: () => false
+            },
+			theme: {
+				type: String,
+                default: () => 'green'
+			},
+			height: {
+				type: Number,
+				default: () => 200
+            },
+			disabled: Boolean
 		},
 		data() {
 			return {
-				leftP: 0,
 				isMouseDown: false,
+				showInputBtn: false,
 				sliderWidth: 0,
-				startX: 0,
-				startP: 0,
 				isShowTip: false,
-				_value: 0
+				endValue: 0,
+				startValue: 0,
+				steps: [],
+				width: 0,
+				left: 0,
+				tipVal: 0,
+				tipLeft: 0,
+				isArray: false
 			}
 		},
 		mounted() {
 			this.setData()
-			this.sliderWidth = this.$refs.slider.offsetWidth
+			this.sliderWidth = this.vertical ? this.$refs.slider.offsetHeight : this.$refs.slider.offsetWidth
 		},
 		methods: {
 			setData() {
-				const {value, max, min} = this
-				this._value = value
+				this.isArray = Array.isArray(this.value)
+				const {value, min, isArray, max, step} = this
+				const steps = []
 
-				let leftP = (value / (max - min) || 0) * 100
-				if (leftP > 100) leftP = 100;
+				for (let i = min; i <= max; i += step) {
+					steps.push(((i -min) / (max - min) || 0) * 100)
+				}
 
-				if (leftP < 0) leftP = 0;
-				this.leftP = leftP
+				this.steps = steps
+
+				if(isArray){
+				  	[this.startValue, this.endValue] = value
+                } else {
+					[this.startValue, this.endValue] = [min, value]
+                }
 			},
-			handleMouseEnter() {
-				this.isShowTip = true
+
+			handleUp(){
+				if (this.disabled) {
+					return false
+				}
+				const {endValue, step, max} = this
+				if(endValue + step < max){
+					this.endValue += step
+				} else {
+					this.endValue = max
+                }
+				this.handleChange()
+            },
+			handleDown(){
+				if (this.disabled) {
+					return false
+				}
+				const {endValue, step, min} = this
+				if(endValue - step > min){
+					this.endValue -= step
+				} else {
+					this.endValue = min
+                }
+
+				this.handleChange()
 			},
-			handleMouseLeave() {
-				if (!this.isMouseDown) this.isShowTip = false;
-			},
-			onDragging(e) {
-				const {startX, isMouseDown, sliderWidth, startP, max, min} = this;
+			handleChange(){
 
-				if (!isMouseDown) return false;
+				const {endValue, max, min, value, startValue, isArray} = this
+				if(endValue > max) this.endValue = max
+				if(endValue < min) this.endValue = min
 
-				let leftP = startP + ((e.screenX - startX) / sliderWidth) * 100;
+				const array = [startValue, endValue]
+				this.left = ((Math.min(...array) - min) / (max - min) || 0) * 100
+				this.width = (((Math.max(...array) - Math.min(...array))) / (max - min) || 0) * 100
 
-				if (leftP > 100) leftP = 100;
-
-				if (leftP < 0) leftP = 0;
-
-				this.leftP = leftP
-
-				this._value = Math.round(min + leftP * (max - min) / 100);
-			},
-			onDragStart(e) {
-				e.preventDefault();
-				this.isMouseDown = true
-				this.startX = e.screenX
-				this.startP = this.leftP
-
-				window.addEventListener('mousemove', this.onDragging);
-				window.addEventListener('mouseup', this.onDragEnd);
-
-			},
-			onDragEnd(e) {
-				e.returnValue = false;
-
-				this.isMouseDown = false;
-				this.isShowTip = false
-				window.removeEventListener('mousemove', this.onDragging);
-				window.removeEventListener('mouseup', this.onDragEnd);
-				this.$emit('input', this._value)
-
-			}
+				if(isArray){
+					this.$emit('input', array)
+					this.$emit('change', array)
+				} else {
+					this.$emit('input', endValue)
+					this.$emit('change', endValue)
+                }
+            },
+			showOrHideTip(val) {
+				[this.isShowTip, this.tipLeft, this.tipVal] = val
+				this.tipVal = parseInt(this.tipVal)
+            }
 		},
 		watch: {
 			value() {
 				this.setData()
+			},
+			startValue() {
+                this.handleChange()
+            },
+			endValue() {
+				this.handleChange()
 			}
 		}
 	}
 </script>
 
-<style>
+<style scoped>
+    .layui-slider {
+        margin-right: 85px;
+    }
+    .layui-slider-input-btn {
+        display: inline-block;
+    }
 
+    .layui-slider-vertical {
+        height: 100%;
+        margin: 45px 30px
+    }
+
+    .layui-slider-tips.is-vertical{
+        margin-bottom: 20px;
+    }
+
+    .layui-slider-input.is-vertical {
+        left: 0;
+        top: 0;
+    }
 </style>
